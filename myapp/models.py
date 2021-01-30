c843ff11650a44c91316dd08ce7612b5b72d8bdc
hrj-star <hetjoshi022@gmail.com>
2021-01-30 15:03:25 +0530

    Initial code

---
+++ myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Team(models.Model):
+    teamname = models.CharField(max_length=30)
+    teamscore = models.IntegerField(default=0)
+
+    def __str__(self):
+    	return self.teamname
